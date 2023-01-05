9c016b4b0daaec2212c85a236d949159e5aa2557
Vidhi Dhama <dhamavidhi12@gmail.com>
2023-01-05 20:42:29 +0530

    path

--- client/src/utils/CONST.js
+++ client/src/utils/CONST.js
@@ -1,5 +1,6 @@
-const API_BASE_PATH = 'http://localhost:4000'
+// const API_BASE_PATH = 'http://localhost:4000'
 // const API_BASE_PATH = 'https://instaclone-app-22.herokuapp.com'
+const API_BASE_PATH = 'https://instaclone-backend-hdsc.onrender.com'
 
 const CONSTANTS = {
     CDN_HOST: '',
